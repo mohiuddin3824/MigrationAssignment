4f8c11a96abc762e9b932a6166cf3915204563d3
H M Mohiuddin <mohiuddin3824@gmail.com>
2023-12-22 12:12:40 +0600

    Migration Update

--- database/migrations/2023_12_22_055816_create_product_reviews_table.php
+++ database/migrations/2023_12_22_055816_create_product_reviews_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('category', 50)->nullable()->after('description');
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('category');
-        });
+        Schema::dropIfExists('product_reviews');
     }
 };
